c3ba627a007f814cbf52834773dd2681c889c9c2
AJMrly <andrewthemorley@gmail.com>
2018-04-19 17:32:48 +1000

    Changed site.php and folder names to make it work on final domains being used.

--- site.php
+++ site.php
@@ -4,14 +4,14 @@
 
 $kirby   = kirby();
 $domain  = server::get('server_name');
-$domains = array('rosnerpi.ajm.im', 'rosner.ajm.im');
+$domains = array('rosner.id.au', 'brian.rosner.id.au');
 
 function is_localhost() {
     $whitelist = array( '127.0.0.1', '::1' );
     if( in_array( $_SERVER['REMOTE_ADDR'], $whitelist) )
         return true;
 }
-if(is_localhost()) $domain = 'rosnerpi.ajm.im';
+if(is_localhost()) $domain = 'rosner.id.au';
 
 if(in_array($domain, $domains)) {
 
