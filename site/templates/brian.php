c5484514e28600f4f24a563bb45b1a5ef7b68ebf
AJMrly <andrewthemorley@gmail.com>
2018-04-23 17:07:43 +1000

    created alternate template and associated yaml/text files for brian subdomain homepage

---
+++ site/templates/brian.php
@@ -0,0 +1,26 @@
+<?php snippet('header') ?>
+
+  <main class="main" role="main">
+    
+    <header class="wrap">
+      <h1><?= $page->title()->html() ?></h1>
+      <div class="intro text">
+        <?= $page->intro()->kirbytext() ?>
+      </div>
+      <hr />
+    </header>
+    <section class="articles-section">
+      
+        <?php snippet('herobook') ?>
+        <?php snippet('showcase', ['limit' => 3]) ?>
+        <p class="articles-section-more"><a href="<?= $page->children()->first() ?>" class="btn">show all &hellip;</a></p>
+      
+    </section>
+    <div class="text wrap">
+      <?= $page->text()->kirbytext() ?>
+    </div>
+  
+
+  </main>
+
+<?php snippet('footer') ?>
