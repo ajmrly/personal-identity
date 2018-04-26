ed006aa4f8a0607a179e687dd15b7ac009dc37d4
AJMrly <andrewthemorley@gmail.com>
2018-04-26 13:11:59 +1000

    updated article tags to take advantage of filtering, updated home template with wrap classes to correctly style showcase section

--- site/templates/home.php
+++ site/templates/home.php
@@ -9,17 +9,21 @@
       </div>
       <hr />
     </header>
-    <section class="articles-section">
+    <section class="articles-section wrap wide">
       
         <?php snippet('herobook') ?>
         <?php snippet('showcase', ['limit' => 3]) ?>
         <p class="articles-section-more"><a href="<?= $page->children()->first() ?>" class="btn">show all &hellip;</a></p>
       
     </section>
+
     <div class="text wrap">
       <?= $page->text()->kirbytext() ?>
     </div>
-  
+    
+    <section class="about-section wrap wide">
+      <!--miniature "about" Brian section -->
+    </section>
 
   </main>
 
